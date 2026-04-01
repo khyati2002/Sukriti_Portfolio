2ee276b85891697b28ed4fde86f7a1b990000229
Sukriti <93239528+Sukriti-m@users.noreply.github.com>
2026-04-01 11:56:18 +0530

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -9,7 +9,8 @@ const Child = ({ icon }: any) => (
             className="animate-spin h-5 w-5 text-white"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
-            viewBox="0 0 24 24">
+            viewBox="0 0 24 24"
+            >
             <circle
                 className="opacity-25"
                 cx="12"
